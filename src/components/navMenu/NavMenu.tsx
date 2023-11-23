3f619074055887f58075a85a1d8d419df8ae7ec0
Siarhei <siarhei.zavadski@gmail.com>
2023-11-23 23:12:14 +0100

    change nav menu buttons

--- src/components/navMenu/NavMenu.tsx
+++ src/components/navMenu/NavMenu.tsx
@@ -1,12 +1,19 @@
+import { IconHomeCircleOutline, IconProfile } from '../../icons/Icons';
 import navigationMenu from './css/NavigationMenu.module.css';
 
 function NavMenu() {
+	const iconSize = '32px';
 	return (
 		<div className={navigationMenu.main}>
 			<div className={navigationMenu.buttons}>
-				<div>Bake</div>
-				<div>Courses</div>
-				<div>Profile</div>
+				<div className={navigationMenu.iconBlock}>
+					<IconHomeCircleOutline height={iconSize} width={iconSize} />
+					Home
+				</div>
+				<div className={navigationMenu.iconBlock}>
+					<IconProfile height={iconSize} width={iconSize} />
+					Profile
+				</div>
 			</div>
 		</div>
 	);
--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -58,3 +58,36 @@ export function IconCalendar3(props: React.SVGProps<SVGSVGElement>) {
 		</svg>
 	);
 }
+
+export function IconProfile(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg fill="none" viewBox="0 0 24 24" height="1em" width="1em" {...props}>
+			<path
+				fill="currentColor"
+				fillRule="evenodd"
+				d="M16 9a4 4 0 11-8 0 4 4 0 018 0zm-2 0a2 2 0 11-4 0 2 2 0 014 0z"
+				clipRule="evenodd"
+			/>
+			<path
+				fill="currentColor"
+				fillRule="evenodd"
+				d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zM3 12c0 2.09.713 4.014 1.908 5.542A8.986 8.986 0 0112.065 14a8.984 8.984 0 017.092 3.458A9 9 0 103 12zm9 9a8.963 8.963 0 01-5.672-2.012A6.992 6.992 0 0112.065 16a6.991 6.991 0 015.689 2.92A8.964 8.964 0 0112 21z"
+				clipRule="evenodd"
+			/>
+		</svg>
+	);
+}
+
+export function IconHomeCircleOutline(props: React.SVGProps<SVGSVGElement>) {
+	return (
+		<svg
+			viewBox="0 0 24 24"
+			fill="currentColor"
+			height="1em"
+			width="1em"
+			{...props}
+		>
+			<path d="M12 20c-4.4 0-8-3.6-8-8s3.6-8 8-8 8 3.6 8 8-3.6 8-8 8m0-18C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2m-1 12h2v3h3v-5h2l-6-5-6 5h2v5h3v-3" />
+		</svg>
+	);
+}
